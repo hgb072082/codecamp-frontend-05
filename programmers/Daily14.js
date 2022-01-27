8afb0f14aebb5caa281070a500e8e6859ab1f79d
Gyubin <hgb072082@gmail.com>
2022-01-27 14:03:31 +0900

    알고리즘끝났지롱~

---
+++ programmers/Daily14.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+    var answer = [];
+    
+for (let i =0;i<numbers.length;i++){
+for(let j=i+1; j<numbers.length;j++)
+if(answer.includes(numbers[i]+numbers[j])){continue;
+}else{answer.push(numbers[i]+numbers[j])}
+
+}
+  answer.sort((a,b)=>(a-b))
+   return answer;
+}
